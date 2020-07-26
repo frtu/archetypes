b52c6987d2a84a25c72f62ef2b3a112190eb875a
Tu, Fred <frederic.tu@gmail.com>
2020-07-26 23:47:11 +0800

    Make a simple graphql server sample

---
+++ plt-graphql-project/src/main/resources/archetype-resources/__rootArtifactId__-server/src/main/java/DataFetchersDelegateQueryImpl.java
@@ -0,0 +1,26 @@
+package ${groupId};
+
+import org.springframework.stereotype.Component;
+
+import ${groupId}.${artifactId}.DataFetchersDelegateQuery;
+
+import graphql.schema.DataFetchingEnvironment;
+
+@Component
+public class DataFetchersDelegateQueryImpl implements DataFetchersDelegateQuery {
+
+	@Override
+	public String hello(DataFetchingEnvironment dataFetchingEnvironment, String name) {
+		return "Hello" + ((name == null) ? "" : " " + name);
+	}
+
+	/**
+	 * Error will always thrown an error. This very interesting query is used to check that errors are properly managed,
+	 * in integration tests.
+	 */
+	@Override
+	public String error(DataFetchingEnvironment dataFetchingEnvironment) {
+		throw new RuntimeException("This is an expected error");
+	}
+
+}
