0d8480e158efa5029acfee8cceafb665d492be90
Tu, Fred <frederic.tu@gmail.com>
2020-07-26 23:53:34 +0800

    Allow test to start

--- plt-graphql-project/src/main/resources/archetype-resources/__rootArtifactId__-server/src/test/java/graphql/SpringBootConsoleApplication.java
+++ plt-graphql-project/src/main/resources/archetype-resources/__rootArtifactId__-server/src/test/java/graphql/SpringBootConsoleApplication.java
@@ -1,21 +1,14 @@
 package ${groupId}.graphql;
 
-import ${groupId}.${artifactId}.${DatamodelClassName};
 import com.github.frtu.logs.tracing.annotation.ExecutionSpan;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
 @Slf4j
 @SpringBootApplication
 public class SpringBootConsoleApplication implements CommandLineRunner {
-    @Autowired
-    private DataModelGraphQLRepository repository;
-
     public static void main(String[] args) {
         SpringApplication.run(SpringBootConsoleApplication.class, args);
     }
@@ -23,7 +16,6 @@ public class SpringBootConsoleApplication implements CommandLineRunner {
     @Override
     @ExecutionSpan
     public void run(String... args) {
-        final List<${DatamodelClassName}> result = repository.find("");
-        LOGGER.info("result:{}", result);
+        LOGGER.info("args:{}", args);
     }
 }
