82ebe3a08b05a612c6afff8f13e39025d9388e76
Tu, Fred <frederic.tu@gmail.com>
2020-08-16 21:51:42 +0800

    Fix compile issue

--- plt-webflux-project/src/main/resources/archetype-resources/src/main/java/Application.java
+++ plt-webflux-project/src/main/resources/archetype-resources/src/main/java/Application.java
@@ -1,7 +1,11 @@
 package ${groupId};
 
+import ${groupId}.repository.DataModelRepository;
+import ${groupId}.repository.DataModel;
 import lombok.extern.slf4j.Slf4j;
 import com.github.frtu.logs.config.LogConfigAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -22,12 +26,10 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         final DataModel model = new DataModel();
-                .withName("name")
-                .withType(${DatamodelClassName}Type.SINGLE)
-                .withValue(10.3F)
-                .withEventTime("2020-08-09")
-                .build();
-        final ${DatamodelClassName} savedModel = repository.save(model);
+        model.setName("name");
+        model.setValue(10.3F);
+        model.setEventTime("2020-08-09");
+        final DataModel savedModel = repository.save(model);
         System.out.println(savedModel.getId());
     }
 }
