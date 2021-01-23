e144166b0df4b98d010c0005f492edf7c06ea5c4
Tu, Fred <frederic.tu@gmail.com>
2021-01-24 01:11:36 +0800

    Allow code to support kotlin

--- kotlin-spring-boot-project/src/main/resources/archetype-resources/src/main/kotlin/Application.kt
+++ kotlin-spring-boot-project/src/main/resources/archetype-resources/src/main/kotlin/Application.kt
@@ -1,20 +1,15 @@
 package ${groupId};
 
-import lombok.extern.slf4j.Slf4j;
-import com.github.frtu.logs.config.LogConfigAll;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@Slf4j
 @SpringBootApplication
-@Import({LogConfigAll.class})
-public class Application {
-    public static void main(String[] args) {
-        try {
-            SpringApplication.run(Application.class, args);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+class Application
+
+fun main(args: Array<String>) {
+    try {
+        SpringApplication.run(Application::class.java, *args)
+    } catch (e: Exception) {
+        e.printStackTrace()
     }
 }
