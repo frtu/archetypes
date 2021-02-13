bf63de8081dc87a158404783704ccdf17299db09
Tu, Fred <frederic.tu@gmail.com>
2021-02-13 10:57:53 +0800

    Add configuration bean

--- kotlin-spring-boot-project/src/main/resources/archetype-resources/src/main/kotlin/Application.kt
+++ kotlin-spring-boot-project/src/main/resources/archetype-resources/src/main/kotlin/Application.kt
@@ -2,10 +2,12 @@ package ${groupId}
 
 import com.github.frtu.logs.config.LogConfigAll
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 
 @Import(LogConfigAll::class)
+@EnableConfigurationProperties(AppProperties::class)
 @SpringBootApplication
 class Application
 
