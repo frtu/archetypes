3bbc61430fe220f0555b2c5ec2d0ecc5b63e26b7
Tu, Fred <frederic.tu@gmail.com>
2021-03-28 23:25:12 +0800

    FIX kotlin-plt-stream-project issues

--- kotlin-plt-stream-project/src/main/resources/archetype-resources/__rootArtifactId__-stream/src/main/kotlin/Application.kt
+++ kotlin-plt-stream-project/src/main/resources/archetype-resources/__rootArtifactId__-stream/src/main/kotlin/Application.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
+import org.springframework.cloud.stream.annotation.EnableBinding
 
 @Import(LogConfigAll::class)
 @EnableConfigurationProperties(AppProperties::class)
