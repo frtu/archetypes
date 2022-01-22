bb3b74f8a2c5c017f8766a57e1c776756d2e063d
Tu, Fred <frederic.tu@gmail.com>
2022-01-22 23:02:45 +0800

    k8s - add integration with kubernetes-maven-plugin

--- kotlin-plt-stream-project/src/main/resources/archetype-resources/__rootArtifactId__-stream/run.bash
+++ kotlin-plt-stream-project/src/main/resources/archetype-resources/__rootArtifactId__-stream/run.bash
@@ -3,11 +3,18 @@ ${artifactId}build() {
   echo "mvn clean package"
   mvn clean package
 }
+echo "-> Run bash individually - Type '${artifactId}k8s' to run the executable"
+${artifactId}k8s() {
+  echo "Running executable > mvn k8s:resource k8s:build"
+  mvn k8s:resource k8s:build
+}
+echo "-> Run bash individually - Type '${artifactId}registryk8s' to run the executable"
+${artifactId}k8sregistry() {
+  echo "Running executable > mvn k8s:resource k8s:build k8s:push -Pregistry-k8s"
+  mvn k8s:resource k8s:build k8s:push -Pregistry-k8s
+}
 echo "-> Run bash individually - Type '${artifactId}run' to run the executable"
 ${artifactId}run() {
   echo "Running executable > mvn spring-boot:run"
   mvn spring-boot:run
 }
-
-${artifactId}build
-${artifactId}run
