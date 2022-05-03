99314613248ee227b2ef3a2934edc08e05234b74
Tu, Fred <frederic.tu@gmail.com>
2022-05-03 12:21:28 +0800

    Bump spring-cloud.version -> 2021.0.2

--- kotlin-plt-stream-project/src/main/resources/archetype-resources/__rootArtifactId__-publisher/build.gradle.kts
+++ kotlin-plt-stream-project/src/main/resources/archetype-resources/__rootArtifactId__-publisher/build.gradle.kts
@@ -1,8 +1,21 @@
+//import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+//import org.springframework.boot.gradle.plugin.SpringBootPlugin
+
 plugins {
     java
     kotlin("jvm")
     application
+
+//    kotlin("plugin.spring") version Versions.kotlin
+//    id("org.springframework.boot") version Versions.spring_boot
 }
+//apply(plugin = "io.spring.dependency-management")
+
+//the<DependencyManagementExtension>().apply {
+//    imports {
+//        mavenBom(SpringBootPlugin.BOM_COORDINATES)
+//    }
+//}
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
