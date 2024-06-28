7b158350f69a71a10e076cf5f6d4fa97ff824b90
Tu, Fred <frederic.tu@gmail.com>
2024-06-29 00:47:49 +0800

    feat: bump spring boot 3x, kotlin 1.9 & jdk 17

--- kotlin-spring-boot-3x-project/src/main/resources/archetype-resources/src/main/kotlin/AppProperties.kt
+++ kotlin-spring-boot-3x-project/src/main/resources/archetype-resources/src/main/kotlin/AppProperties.kt
@@ -1,9 +1,7 @@
 package ${groupId}
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 
-@ConstructorBinding
 @ConfigurationProperties("prefix")
 data class AppProperties(var key1: String) {
     data class App(val key1: String? = null)
