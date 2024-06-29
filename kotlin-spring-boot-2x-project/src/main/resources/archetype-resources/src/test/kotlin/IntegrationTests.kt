2cf93e23ad22338660d5a4d8a1fbffcde44d766e
Tu, Fred <frederic.tu@gmail.com>
2024-06-29 15:03:11 +0800

    feat: update to kotest bom & fix UT

--- kotlin-spring-boot-2x-project/src/main/resources/archetype-resources/src/test/kotlin/IntegrationTests.kt
+++ kotlin-spring-boot-2x-project/src/main/resources/archetype-resources/src/test/kotlin/IntegrationTests.kt
@@ -20,7 +20,7 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
     @Test
     fun `Assert url and status code`() {
         println(">> Assert url and status code")
-        val entity = restTemplate.getForEntity<String>("/v1/resources/1234")
+        val entity = restTemplate.getForEntity<String>("/v1/emails")
         entity.statusCode shouldBe HttpStatus.OK
     }
 
